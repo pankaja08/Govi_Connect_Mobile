34495fe102a3c9c0b1d67274b07c414013c63168
IT24103504 <it24103504@my.sliit.lk>
2026-05-03 09:04:06 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,6 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, '[IP_ADDRESS]', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}...`);
 });
