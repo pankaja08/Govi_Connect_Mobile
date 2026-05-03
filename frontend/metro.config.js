272c1316ba96827e3436cfb10cb1fa8247e74c84
IT24103504 <it24103504@my.sliit.lk>
2026-05-03 20:58:50 +0530

    allow metro to access nested node_modules"

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -8,7 +8,6 @@ const config = getDefaultConfig(__dirname);
 // We explicitly block these prefixes to prevent Metro from attempting to cache them.
 
 config.resolver.blockList = [
-  /.*node_modules\/.*\/node_modules\/.*/, // Dedupe nodes
   /.*node:sea.*/,                         // Specifically block problematic Node internal
   /\.expo\/.*/,                           // Ignore .expo folder
 ];
